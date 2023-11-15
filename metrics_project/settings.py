b6e4cf5020c6aa7551ba21ea3c48b4f90293cbcd
Viktor Salanki <viktorsalanki@gmail.com>
2023-11-14 22:51:28 -0500

    Add cors_allowed_origin for frontend

--- metrics_project/settings.py
+++ metrics_project/settings.py
@@ -27,6 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# SECURITY WARNING: don't run with allow all origins in production!
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
 
 # Application definition
 
@@ -39,11 +43,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'metrics',
     'stats',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
