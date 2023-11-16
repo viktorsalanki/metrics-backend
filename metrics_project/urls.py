24ce6905cc2f2aab69bef218ed06aad00a41cec6
Viktor Salanki <viktorsalanki@gmail.com>
2023-11-15 22:07:10 -0500

    Set api/v1/metrics as root

--- metrics_project/urls.py
+++ metrics_project/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='api/v1/metrics/', permanent=False)),
     path('admin/', admin.site.urls),
     path('api/v1/metrics/', include('metrics.urls')),
     path('api/v1/stats/', include('stats.urls')),
